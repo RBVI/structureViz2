bf8cb744d5a295cf3f23065f29dc26882ca7075a
Nadezhda Doncheva <doncheva@cgl.ucsf.edu>
2013-04-16 19:24:27 -0700

    Added listener for network delted

---
+++ src/main/java/edu/ucsf/rbvi/structureViz2/internal/model/CyNetworkListener.java
@@ -0,0 +1,18 @@
+package edu.ucsf.rbvi.structureViz2.internal.model;
+
+import org.cytoscape.model.events.NetworkAboutToBeDestroyedEvent;
+import org.cytoscape.model.events.NetworkAboutToBeDestroyedListener;
+
+public class CyNetworkListener implements NetworkAboutToBeDestroyedListener {
+
+	private StructureManager structureManager;
+
+	public CyNetworkListener(StructureManager structureManager) {
+		this.structureManager = structureManager;
+	}
+
+	public void handleEvent(NetworkAboutToBeDestroyedEvent e) {
+		structureManager.updateMapping(e.getNetwork());
+	}
+
+}

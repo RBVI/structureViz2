70a570cb9a5f0c2cd239061e463377beac9b2a2a
doncheva <doncheva@mpi-inf.mpg.de>
2013-06-10 12:20:57 +0200

    Added task factories for open and close structure for edges.

--- src/main/java/edu/ucsf/rbvi/structureViz2/internal/tasks/CloseStructuresEdgeTaskFactory.java
+++ src/main/java/edu/ucsf/rbvi/structureViz2/internal/tasks/CloseStructuresEdgeTaskFactory.java
@@ -3,12 +3,12 @@ package edu.ucsf.rbvi.structureViz2.internal.tasks;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.cytoscape.model.CyEdge;
 import org.cytoscape.model.CyIdentifiable;
 import org.cytoscape.model.CyNetwork;
-import org.cytoscape.model.CyNode;
 import org.cytoscape.model.CyTableUtil;
+import org.cytoscape.task.EdgeViewTaskFactory;
 import org.cytoscape.task.NetworkViewTaskFactory;
-import org.cytoscape.task.NodeViewTaskFactory;
 import org.cytoscape.view.model.CyNetworkView;
 import org.cytoscape.view.model.View;
 import org.cytoscape.work.AbstractTaskFactory;
@@ -16,12 +16,12 @@ import org.cytoscape.work.TaskIterator;
 
 import edu.ucsf.rbvi.structureViz2.internal.model.StructureManager;
 
-public class CloseStructuresTaskFactory extends AbstractTaskFactory implements NodeViewTaskFactory,
+public class CloseStructuresEdgeTaskFactory extends AbstractTaskFactory implements EdgeViewTaskFactory,
 		NetworkViewTaskFactory {
 
 	private StructureManager structureManager;
 
-	public CloseStructuresTaskFactory(StructureManager structureManager) {
+	public CloseStructuresEdgeTaskFactory(StructureManager structureManager) {
 		this.structureManager = structureManager;
 	}
 
@@ -32,18 +32,18 @@ public class CloseStructuresTaskFactory extends AbstractTaskFactory implements N
 	public boolean isReady(CyNetworkView netView) {
 		// Get all of the selected nodes
 		List<CyIdentifiable> selectedList = new ArrayList<CyIdentifiable>();
-		selectedList.addAll(CyTableUtil.getNodesInState(netView.getModel(), CyNetwork.SELECTED, true));
+		selectedList.addAll(CyTableUtil.getEdgesInState(netView.getModel(), CyNetwork.SELECTED, true));
 		if (structureManager.getOpenChimObjNames(selectedList).size() > 0) {
 			return true;
 		}
 		return false;
 	}
 
-	public boolean isReady(View<CyNode> nodeView, CyNetworkView netView) {
+	public boolean isReady(View<CyEdge> edgeView, CyNetworkView netView) {
 		// Get all of the selected nodes
 		List<CyIdentifiable> selectedList = new ArrayList<CyIdentifiable>();
-		selectedList.add(nodeView.getModel());
-		selectedList.addAll(CyTableUtil.getNodesInState(netView.getModel(), CyNetwork.SELECTED, true));
+		selectedList.add(edgeView.getModel());
+		selectedList.addAll(CyTableUtil.getEdgesInState(netView.getModel(), CyNetwork.SELECTED, true));
 		if (structureManager.getOpenChimObjNames(selectedList).size() > 0) {
 			return true;
 		}
@@ -53,15 +53,15 @@ public class CloseStructuresTaskFactory extends AbstractTaskFactory implements N
 	public TaskIterator createTaskIterator(CyNetworkView netView) {
 		// Get all of the selected nodes
 		List<CyIdentifiable> selectedList = new ArrayList<CyIdentifiable>();
-		selectedList.addAll(CyTableUtil.getNodesInState(netView.getModel(), CyNetwork.SELECTED, true));
+		selectedList.addAll(CyTableUtil.getEdgesInState(netView.getModel(), CyNetwork.SELECTED, true));
 		return new TaskIterator(new CloseStructuresTask(selectedList, netView, structureManager));
 	}
 
-	public TaskIterator createTaskIterator(View<CyNode> nodeView, CyNetworkView netView) {
+	public TaskIterator createTaskIterator(View<CyEdge> nodeView, CyNetworkView netView) {
 		// Get all of the selected nodes
 		List<CyIdentifiable> selectedList = new ArrayList<CyIdentifiable>();
 		selectedList.add(nodeView.getModel());
-		selectedList.addAll(CyTableUtil.getNodesInState(netView.getModel(), CyNetwork.SELECTED, true));
+		selectedList.addAll(CyTableUtil.getEdgesInState(netView.getModel(), CyNetwork.SELECTED, true));
 		return new TaskIterator(new CloseStructuresTask(selectedList, netView, structureManager));
 	}
 
--- src/main/java/edu/ucsf/rbvi/structureViz2/internal/tasks/OpenStructuresEdgeTaskFactory.java
+++ src/main/java/edu/ucsf/rbvi/structureViz2/internal/tasks/OpenStructuresEdgeTaskFactory.java
@@ -3,12 +3,12 @@ package edu.ucsf.rbvi.structureViz2.internal.tasks;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.cytoscape.model.CyEdge;
 import org.cytoscape.model.CyIdentifiable;
 import org.cytoscape.model.CyNetwork;
-import org.cytoscape.model.CyNode;
 import org.cytoscape.model.CyTableUtil;
+import org.cytoscape.task.EdgeViewTaskFactory;
 import org.cytoscape.task.NetworkViewTaskFactory;
-import org.cytoscape.task.NodeViewTaskFactory;
 import org.cytoscape.view.model.CyNetworkView;
 import org.cytoscape.view.model.View;
 import org.cytoscape.work.AbstractTaskFactory;
@@ -17,12 +17,12 @@ import org.cytoscape.work.TaskIterator;
 import edu.ucsf.rbvi.structureViz2.internal.model.StructureManager;
 import edu.ucsf.rbvi.structureViz2.internal.model.StructureManager.ModelType;
 
-public class OpenStructuresTaskFactory extends AbstractTaskFactory implements
-		NetworkViewTaskFactory, NodeViewTaskFactory {
+public class OpenStructuresEdgeTaskFactory extends AbstractTaskFactory implements
+		NetworkViewTaskFactory, EdgeViewTaskFactory {
 
 	private StructureManager structureManager;
 
-	public OpenStructuresTaskFactory(StructureManager structureManager) {
+	public OpenStructuresEdgeTaskFactory(StructureManager structureManager) {
 		this.structureManager = structureManager;
 	}
 
@@ -33,7 +33,7 @@ public class OpenStructuresTaskFactory extends AbstractTaskFactory implements
 	public boolean isReady(CyNetworkView netView) {
 		// Get all of the selected nodes/edges
 		List<CyIdentifiable> selectedList = new ArrayList<CyIdentifiable>();
-		selectedList.addAll(CyTableUtil.getNodesInState(netView.getModel(), CyNetwork.SELECTED,
+		selectedList.addAll(CyTableUtil.getEdgesInState(netView.getModel(), CyNetwork.SELECTED,
 				true));
 		if (structureManager.getChimObjNames(netView.getModel(), selectedList, ModelType.SMILES,
 				false).size() > 0
@@ -44,10 +44,10 @@ public class OpenStructuresTaskFactory extends AbstractTaskFactory implements
 		return false;
 	}
 
-	public boolean isReady(View<CyNode> nodeView, CyNetworkView netView) {
+	public boolean isReady(View<CyEdge> edgeView, CyNetworkView netView) {
 		// Get all of the selected nodes
 		List<CyIdentifiable> selectedList = new ArrayList<CyIdentifiable>();
-		selectedList.add(nodeView.getModel());
+		selectedList.add(edgeView.getModel());
 		selectedList.addAll(CyTableUtil.getNodesInState(netView.getModel(), CyNetwork.SELECTED,
 				true));
 		if (structureManager.getChimObjNames(netView.getModel(), selectedList, ModelType.SMILES,
@@ -62,16 +62,16 @@ public class OpenStructuresTaskFactory extends AbstractTaskFactory implements
 	public TaskIterator createTaskIterator(CyNetworkView netView) {
 		// Get all of the selected nodes
 		List<CyIdentifiable> selectedList = new ArrayList<CyIdentifiable>();
-		selectedList.addAll(CyTableUtil.getNodesInState(netView.getModel(), CyNetwork.SELECTED,
+		selectedList.addAll(CyTableUtil.getEdgesInState(netView.getModel(), CyNetwork.SELECTED,
 				true));
 		return new TaskIterator(new OpenStructuresTask(selectedList, netView, structureManager));
 	}
 
-	public TaskIterator createTaskIterator(View<CyNode> nodeView, CyNetworkView netView) {
+	public TaskIterator createTaskIterator(View<CyEdge> edgeView, CyNetworkView netView) {
 		// Get all of the selected nodes
 		List<CyIdentifiable> selectedList = new ArrayList<CyIdentifiable>();
-		selectedList.add(nodeView.getModel());
-		selectedList.addAll(CyTableUtil.getNodesInState(netView.getModel(), CyNetwork.SELECTED,
+		selectedList.add(edgeView.getModel());
+		selectedList.addAll(CyTableUtil.getEdgesInState(netView.getModel(), CyNetwork.SELECTED,
 				true));
 		return new TaskIterator(new OpenStructuresTask(selectedList, netView, structureManager));
 	}

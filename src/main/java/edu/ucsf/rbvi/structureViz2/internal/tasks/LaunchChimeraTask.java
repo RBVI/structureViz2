1e34f7b8f6e92962893d403f502ad3c07f35efd0
doncheva <doncheva@mpi-inf.mpg.de>
2013-07-10 14:51:49 +0200

    Fixed a bug in getting chimera paths when a network is open.

--- src/main/java/edu/ucsf/rbvi/structureViz2/internal/tasks/LaunchChimeraTask.java
+++ src/main/java/edu/ucsf/rbvi/structureViz2/internal/tasks/LaunchChimeraTask.java
@@ -2,6 +2,7 @@ package edu.ucsf.rbvi.structureViz2.internal.tasks;
 
 import java.util.List;
 
+import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.work.AbstractTask;
 import org.cytoscape.work.TaskMonitor;
 
@@ -19,7 +20,9 @@ public class LaunchChimeraTask extends AbstractTask {
 	public void run(TaskMonitor taskMonitor) throws Exception {
 		taskMonitor.setTitle("Launch Chimera");
 		taskMonitor.setStatusMessage("Launching Chimera ...");
-		List<String> pathList = structureManager.getChimeraPaths(null);
+		CyApplicationManager cyAppManager = (CyApplicationManager) structureManager
+				.getService(CyApplicationManager.class);
+		List<String> pathList = structureManager.getChimeraPaths(cyAppManager.getCurrentNetwork());
 
 		structureManager.getChimeraManager().launchChimera(pathList);
 		if (structureManager.getChimeraManager().isChimeraLaunched()) {
